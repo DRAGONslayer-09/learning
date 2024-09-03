b86d8909497018e382e8ae2c7d9f48b99fcc5814
tanis <tanishqchopra143@gmail.com>
2024-09-03 23:11:14 +0530

    initial commit

--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/SizeofLL.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/SizeofLL.java
@@ -1,14 +1,7 @@
 package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class SizeofLL {
-    class Node {
-        int data;
-        Node next;
-        Node(int x){
-            this.data = x;
-            this.next = null;
-        }
-    }
+
     static void add(Node head, int val){
 
     }
