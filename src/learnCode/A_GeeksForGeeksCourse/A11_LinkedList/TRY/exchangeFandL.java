af05effd1598fa2727a4a8e26ab860c25a960693
tanis <tanishqchopra143@gmail.com>
2024-09-04 23:12:38 +0530

    initial commit

--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/exchangeFandL.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/exchangeFandL.java
@@ -2,7 +2,14 @@ package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class exchangeFandL {
     // exchange first and last nodes
-
+    class Node9{
+        int value;
+        RemoveNode.Node next;
+        Node9(int x){
+            value = x;
+            next = null;
+        }
+    }
     public static void main(String[] args) {
 
     }
