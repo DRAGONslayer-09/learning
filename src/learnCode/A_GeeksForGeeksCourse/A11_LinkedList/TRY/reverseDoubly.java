6240305162cef54d7dfe94ad22ec73b6889dd50d
tanis <tanishqchopra143@gmail.com>
2024-09-04 23:15:55 +0530

    initial commit

--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/reverseDoubly.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/reverseDoubly.java
@@ -2,7 +2,14 @@ package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class reverseDoubly {
     // reverse the doubly linked list
-
+    class Nodet{
+        int value;
+        RemoveNode.Node next;
+        Nodet(int x){
+            value = x;
+            next = null;
+        }
+    }
     public static void main(String[] args) {
 
     }
