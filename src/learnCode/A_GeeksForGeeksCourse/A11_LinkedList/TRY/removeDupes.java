db90329f4a9067df25ee5bfa8f5cb8fd54e2312d
tanis <tanishqchopra143@gmail.com>
2024-09-04 23:11:38 +0530

    initial commit

--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/removeDupes.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/removeDupes.java
@@ -1,6 +1,14 @@
 package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class removeDupes {
+    class Nodem{
+        int value;
+        RemoveNode.Node next;
+        Nodem(int x){
+            value = x;
+            next = null;
+        }
+    }
 
     public static void main(String[] args) {
 
--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/sortLL.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/sortLL.java
@@ -2,6 +2,7 @@ package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class sortLL {
 
+
     public static void main(String[] args) {
 
     }
