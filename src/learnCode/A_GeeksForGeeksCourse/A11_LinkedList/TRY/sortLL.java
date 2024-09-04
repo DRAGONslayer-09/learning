d8f155872477f7bbbe6f18508063b1f6fed35007
tanis <tanishqchopra143@gmail.com>
2024-09-04 23:12:08 +0530

    initial commit

--- src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/sortLL.java
+++ src/learnCode/A_GeeksForGeeksCourse/A11_LinkedList/TRY/sortLL.java
@@ -1,7 +1,14 @@
 package learnCode.A_GeeksForGeeksCourse.A11_LinkedList.TRY;
 
 public class sortLL {
-
+    class Nodem{
+        int value;
+        RemoveNode.Node next;
+        Nodem(int x){
+            value = x;
+            next = null;
+        }
+    }
 
     public static void main(String[] args) {
 
